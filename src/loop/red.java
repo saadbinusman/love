7f40d00d33db02c58fd059215f2734a4b5e4d778
Saad bis Usman <test@saad.com>
2017-05-27 13:06:17 +0500

    1commit

---
+++ src/loop/red.java
@@ -0,0 +1,15 @@
+package loop;
+
+public class red {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int a=0; a<=10; a++)
+		{
+			System.out.println("saad" + a);
+		}
+
+	}
+
+}
